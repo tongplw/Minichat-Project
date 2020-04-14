61926e94df8cb37c18e586e0afe73bf8c2268cbe
tongplw <bangkok.tong@gmail.com>
2020-04-15 04:45:49 +0700

    update database.py

--- minichat/database.py
+++ minichat/database.py
@@ -29,24 +29,24 @@ def create_group(name):
 def load_channels():
     cmd = f'SELECT * FROM minichat.groups;'
     with db.connect() as conn:
-        conn.execute(cmd)
-        return [i[0] for i in conn.fetchall()]
+        result = conn.execute(cmd)
+        return [i[0] for i in result.fetchall()]
 
 def load_users():
     cmd = f'SELECT * FROM minichat.users;'
     with db.connect() as conn:
-        conn.execute(cmd)
-        return [i[0] for i in conn.fetchall()]
+        result = conn.execute(cmd)
+        return [i[0] for i in result.fetchall()]
 
 def load_channels_history():
     cmd = f'SELECT * FROM minichat.message;'
     with db.connect() as conn:
-        conn.execute(cmd)
+        result = conn.execute(cmd)
         
-        result = {}
-        for record in conn.fetchall():
+        history = {}
+        for record in result.fetchall():
             message, username, channel = record[1:4]
-            if channel not in result:
-                result[channel] = []
-            result[channel].append((username, message))
-        return result
+            if channel not in history:
+                history[channel] = []
+            history[channel].append((username, message))
+        return history

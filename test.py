0001f2ea9ab0916bb9d1be3cb2bf11d68c785b33
tongplw <bangkok.tong@gmail.com>
2020-04-30 00:00:39 +0700

    Delete unused file

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-
-def can_login(is_online, last_login):
-    if last_login < datetime.now() - timedelta(hours=1) or not is_online:
-        return True
-    return False

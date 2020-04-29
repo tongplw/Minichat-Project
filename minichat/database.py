17f59962b04b86c3c97b804107f99da00264658a
tongplw <bangkok.tong@gmail.com>
2020-04-29 19:12:12 +0700

    fix datetime

--- minichat/database.py
+++ minichat/database.py
@@ -71,7 +71,7 @@ def can_login(username):
     with db.connect() as conn:
         result = conn.execute(cmd)
     is_online, last_login = result.fetchone()
-    if last_login < date.today() - timedelta(hours=1) or not is_online:
+    if last_login < datetime.now() - timedelta(hours=1) or not is_online:
         return True
     return False
 

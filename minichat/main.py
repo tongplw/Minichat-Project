cbbf62b5f5cc5ed1c8a6ec5f336b6492478d8d57
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-05-01 20:54:35 +0700

    init changes

--- minichat/main.py
+++ minichat/main.py
@@ -15,7 +15,7 @@ channel_list = db.load_channels()
 history = db.load_channels_history()
 
 channel_template = """
-<li class="{is_active}">
+<li class="{is_active}" onClick=joinGroup({})>
     <div class="d-flex bd-highlight">
         <div class="user_info">
             <span>{channel_name}</span>
